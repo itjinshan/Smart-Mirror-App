4ea5eeff37d87f700028ab7146bfbcae6ff84b57
Alex Jiang <it.jinshan@gmail.com>
2019-07-14 21:37:37 -0700

    profile picture ready, timely greeting ready but need CSS

--- src/ProfileScreen/ProfileScreen.js
+++ src/ProfileScreen/ProfileScreen.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View,Button} from 'react-native';  
+import {StyleSheet,
+        Text,
+        View,
+        Button,
+        Image} from 'react-native';  
 import { Header } from 'react-native-elements';
+import moment from 'moment';
 
 class ProfileScreen extends React.Component {  
+  constructor(props) {
+    super(props);
+    this.state = {
+      //defauilt value of the time
+      time: '',
+      greeting: '',
+    };
+  }
+  componentDidMount() {
+//Getting the current date-time with required formate and UTC   
+    var hour = moment()
+      .format('HH');
+
+    this.setState({ time: hour });
+    //Settign up time to show
+    if(hour < 12){
+      this.setState({greeting: 'Good Morning!'})
+    }
+    else if(hour > 12 && hour < 18){
+      this.setState({greeting: 'Good Afternoon!'})
+    }
+    else{
+      this.setState({greeting: 'Good Evening!'})
+    }
+  }
     render() {  
       return (  
           <View style={styles.container}>  
@@ -13,12 +43,11 @@ class ProfileScreen extends React.Component {
               />
             </View>
             <View style={styles.top}>
-              <View style={styles.profileImg}>
-
-              </View>
+                <Image style={styles.profileImg} 
+                       source={{uri:'https://freehindistatus.com/wp-content/uploads/2018/05/cute-baby-whatsapp-profile-300x300.jpg'}} />
             </View> 
             <View style={styles.center}>
-
+              <Text>{this.state.greeting}</Text>
             </View>
             <View style={styles.bottom}>
               
@@ -33,7 +62,7 @@ class ProfileScreen extends React.Component {
         flex: 1
     },
     top: {
-      height: '45%',
+      height: '25%',
       alignItems: 'center',
       justifyContent: 'center',
       backgroundColor: '#98d2c1',
@@ -43,7 +72,7 @@ class ProfileScreen extends React.Component {
       backgroundColor: '#7fbcac',
     },
     bottom: {
-      height: '45%',
+      height: '65%',
       backgroundColor: '#fff',
       flexDirection: 'row',
       flexWrap: 'wrap',
@@ -52,7 +81,7 @@ class ProfileScreen extends React.Component {
     profileImg: {
       width: 140,
       height: 140,
-      borderRadius: 100,
+      borderRadius: 70,
       borderWidth: 4,
       borderColor: '#fff',
       backgroundColor: '#eee',
